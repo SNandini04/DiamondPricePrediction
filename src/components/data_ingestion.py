1f10f398c91c9e43baacdf669bd4090c6e1e8683
SNandini04 <nandani-4-8@gmail.com>
2023-04-20 14:11:35 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,7 +8,6 @@ from dataclasses import dataclass
 
 from data_transformation import DataTransformation
 
-
 ## Intitialize the Data Ingetion Configuration
 
 @dataclass
@@ -48,8 +47,3 @@ class DataIngestion:
             logging.info('Exception occured at Data Ingestion stage')
             raise CustomException(e,sys)
         
-if __name__=='__main__':
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
